8b84779ee04bb12e1fd963a278b207007cc9fc17
shurm <michaelshur28@gmail.com>
2019-09-05 15:17:25 -0400

    alien bullets spawn at lowest level

--- Assets/ArmadaAttackController.cs
+++ Assets/ArmadaAttackController.cs
@@ -20,12 +20,30 @@ public class ArmadaAttackController : MonoBehaviour
     {
         if (timeRemaining <= 0)
         {
-            
+            int randomAlienColumn = Random.Range(0, transform.childCount-1);
+
+            Transform columnChosen = transform.GetChild(randomAlienColumn);
+            Transform lowestAlienInColumn = GetChildWithLowestY(columnChosen);
+
+            Instantiate(bulletPrefab, lowestAlienInColumn.position + Vector3.down * bulletSpawnDistatnce, Quaternion.identity);
+
             timeRemaining = attackTimeInterval;
         }
         else
         {
             timeRemaining -= Time.deltaTime;
         }
+    }
+    Transform GetChildWithLowestY(Transform column)
+    {
+        Transform lowestChild = column.GetChild(0);
+        for(int i=1;i<column.childCount;i++)
+        {
+            if(lowestChild.position.y > column.GetChild(1).position.y)
+            {
+                lowestChild = column.GetChild(1);
+            }
+        }
+        return lowestChild;
     }
 }
