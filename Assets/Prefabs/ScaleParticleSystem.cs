98cc69cc14b6cc1478d5d319241a787b04d7d8a7
shurm <michaelshur28@gmail.com>
2019-09-14 18:26:54 -0400

    rocket with trail created

---
+++ Assets/Prefabs/ScaleParticleSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleParticleSystem : MonoBehaviour
+{
+    private ParticleSystem ps;
+    public float sliderValue = 1.0F;
+    public float parentSliderValue = 1.0F;
+    public ParticleSystemScalingMode scaleMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ParticleSystem.MainModule main = ps.main;
+        main.scalingMode = scaleMode;
+    }
+}
