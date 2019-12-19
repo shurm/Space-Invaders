bc824dc9a4520e972878ef29a0c8d23c7dcd5796
shurm <michaelshur28@gmail.com>
2019-12-19 16:46:27 -0500

    added 3d aliens using primitives

--- Assets/Scripts/ArmadaChildSpawner.cs
+++ Assets/Scripts/ArmadaChildSpawner.cs
@@ -12,29 +12,45 @@ public class ArmadaChildSpawner : MonoBehaviour
     public int columns;
 
     public int rows;
+
     // Start is called before the first frame update
     void Start()
     {
         int duplicateRows = rows / alienPrefabs.Length;
         GameObject emptyColumn = new GameObject("column");
 
-        float leftBoundary = -1 * columns / 2 * columnSpacing;
-        if (columns % 2 == 1)
-            leftBoundary -= columnSpacing / 2;
+        float leftBoundaryX = -1 * columns / 2 * columnSpacing;
+        if (columns % 2 == 0)
+            leftBoundaryX += columnSpacing / 2;
 
-        Vector3 leftSpawnPos = new Vector3(leftBoundary, 0, 0);
 
-        for (int c = 0;c<columns;c++)
+        float leftBoundaryY = -1 * rows / 2 * rowSpacing;
+        if (rows % 2 == 0)
+            leftBoundaryY += rowSpacing / 2;
+
+        Vector3 leftSpawnPos = new Vector3(leftBoundaryX, 0, 0);
+
+        Vector3 bottomSpawnPos = new Vector3(0, leftBoundaryY, 0);
+
+
+        for (int c = 0; c < columns; c++)
         {
-            GameObject newColumn = GameObject.Instantiate(emptyColumn, Vector3.zero, Quaternion.identity, transform);
-            newColumn.transform.localPosition = leftSpawnPos + Vector3.right * columnSpacing*c;
+            Vector3 pos = transform.position + leftSpawnPos + Vector3.right * columnSpacing * c;
 
+            GameObject newColumn = GameObject.Instantiate(emptyColumn, pos, Quaternion.identity, transform);
 
+            for (int r = 0; r < alienPrefabs.Length; r++)
+            {
+                for (int d = 0; d < duplicateRows; d++)
+                {
+                    pos = transform.position + leftSpawnPos + Vector3.right * columnSpacing * c + bottomSpawnPos + Vector3.up * rowSpacing * (duplicateRows * r + d);
+                    Instantiate(alienPrefabs[alienPrefabs.Length - 1 - r], pos, Quaternion.identity, newColumn.transform);
+                }
+            }
         }
 
         Destroy(emptyColumn);
 
     }
 
-   
 }
