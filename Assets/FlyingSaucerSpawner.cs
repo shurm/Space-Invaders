bceb9db2bf14d7950d3638c8c72c05c4ca7f4190
shurm <michaelshur28@gmail.com>
2020-01-04 00:53:09 -0500

    corrected ui

---
+++ Assets/FlyingSaucerSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingSaucerSpawner : MonoBehaviour
+{
+    public int maxIntervalInSeconds;
+    public GameObject flyingSaucerPrefab;
+
+    //private float timeToSpawn
+    // Start is called before the first frame update
+    void Start()
+    {
+       // public int maxIntervalInSeconds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
